11e4b32561c63b730dff48cae337eee8d8fbe15a
Andrey Vasin <and.vas074@gmail.com>
2016-05-29 23:43:31 +0500

    +Interface HasEmail

--- App/Model.php
+++ App/Model.php
@@ -2,8 +2,7 @@
 
 namespace App;
 
-
-class Model
+abstract class Model
 {
   const TABLE = '';
 
--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\User;
+
 require __DIR__ . '/autoload.php';
 
 $users = \App\Models\User::findAll();
 
-var_dump($users);
+//var_dump($users[0]->getEmail());
+function sendEmail(\App\Models\HasEmail $user, string $massage)
+{
+  echo "Почта уходит на " . $user->email;
+}
+
+sendEmail($users[0], 'Hello!');
