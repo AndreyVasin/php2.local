d630fb40563d5f9a9f93e5aa1382e7c40c3cb8da
Andrey Vasin <and.vas074@gmail.com>
2016-05-30 01:13:48 +0500

    ! ActiveRecord

--- App/Model.php
+++ App/Model.php
@@ -5,6 +5,7 @@ namespace App;
 abstract class Model
 {
   const TABLE = '';
+  public $id;
 
   public static function findAll()
   {
@@ -12,5 +13,35 @@ abstract class Model
     return $db->query(
       'SELECT * FROM ' . static::TABLE,
       static::class);
+  }
+
+  public function isNew()
+  {
+    return empty($this->id);
+  }
+
+  public function insert()
+  {
+    if (!$this->isNew()){
+      return;
+    }
+    $columns = [];
+    $values = [];
+    foreach ($this as $k => $v){
+      if ('id' == $k){
+        continue;
+      }
+      $columns[] = $k;
+      $values[':'.$k] = $v;
+    }
+    //var_dump($values);
+
+    $sql = 'INSERT INTO ' . static::TABLE . ' 
+            (' . implode(',', $columns) . ')
+            VALUES
+            (' . implode(',', array_keys($values)) . ')
+            ';
+    $db = Db::instance();
+    $db->execute($sql, $values); 
   }
 }
--- index.php
+++ index.php
@@ -4,12 +4,7 @@ use App\Models\User;
 
 require __DIR__ . '/autoload.php';
 
-$users = \App\Models\User::findAll();
-
-//var_dump($users[0]->getEmail());
-function sendEmail(\App\Models\HasEmail $user, string $massage)
-{
-  echo "Почта уходит на " . $user->email;
-}
-
-sendEmail($users[0], 'Hello!');
+$user = new User();
+$user->name = 'Vasya';
+$user->email = 'v@pupkin.ru';
+$user->insert();
