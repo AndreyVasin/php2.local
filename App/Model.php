79f2c7186813d081a9a06f85d74aef9fc519d2d7
Andrey Vasin <and.vas074@gmail.com>
2016-06-07 15:15:45 +0500

    + spl_autoload_register

--- App/Model.php
+++ App/Model.php
@@ -4,91 +4,91 @@ namespace App;
 
 abstract class Model
 {
-  const TABLE = '';
-  public $id;
+    const TABLE = '';
+    public $id;
 
-  public static function findAll()
-  {
-    $db = Db::instance();
-    return $db->query(
-      'SELECT * FROM ' . static::TABLE,
-      static::class);
-  }
-
-  public static function findById(int $id)
-  {
-    $db = Db::instance();
-    return $db->query(
-      'SELECT * FROM ' . static::TABLE . ' WHERE id = :id',
-      static::class, [':id' => $id])[0];
-  }
-
-  public function isNew()
-  {
-    return empty($this->id);
-  }
-
-  public function insert()
-  {
-    if (!$this->isNew()) {
-      return;
-    }
-    $columns = [];
-    $values = [];
-    foreach ($this as $k => $v) {
-      if ('id' == $k) {
-        continue;
-      }
-      $columns[] = $k;
-      $values[':' . $k] = $v;
+    public static function findAll()
+    {
+        $db = Db::instance();
+        return $db->query(
+            'SELECT * FROM ' . static::TABLE,
+            static::class);
     }
 
-    $sql = 'INSERT INTO ' . static::TABLE . ' 
+    public static function findById(int $id)
+    {
+        $db = Db::instance();
+        return $db->query(
+            'SELECT * FROM ' . static::TABLE . ' WHERE id = :id',
+            static::class, [':id' => $id])[0];
+    }
+
+    public function isNew()
+    {
+        return empty($this->id);
+    }
+
+    public function insert()
+    {
+        if (!$this->isNew()) {
+            return;
+        }
+        $columns = [];
+        $values = [];
+        foreach ($this as $k => $v) {
+            if ('id' == $k) {
+                continue;
+            }
+            $columns[] = $k;
+            $values[':' . $k] = $v;
+        }
+
+        $sql = 'INSERT INTO ' . static::TABLE . ' 
             (' . implode(',', $columns) . ')
             VALUES
             (' . implode(',', array_keys($values)) . ')
             ';
-    $db = Db::instance();
-    $db->execute($sql, $values);
-  }
+        $db = Db::instance();
+        $db->execute($sql, $values);
+    }
 
-  public function delete()
-  {
-    if (!$this->isNew()) {
-      return;
+    public function delete()
+    {
+        if (!$this->isNew()) {
+            return;
+        }
+        $currId = 0;
+        foreach ($this as $k => $v) {
+            if ('id' == $k) {
+                $currId = (int)$v;
+            }
+        }
+        $sql = 'DELETE FROM ' . static::TABLE .
+            ' WHERE id = ' . $currId;
+        $db = Db::instance();
+        $db->execute($sql);
     }
-    $currId = 0;
-    foreach ($this as $k => $v) {
-      if ('id' == $k) {
-        $currId = (int)$v;
-      }
-    }
-    $sql = 'DELETE FROM ' . static::TABLE .
-      ' WHERE id = ' . $currId;
-    $db = Db::instance();
-    $db->execute($sql);
-  }
 
-  public function update()
-  {
-    if (!$this->isNew()) {
-      return;
+    public function update()
+    {
+        if (!$this->isNew()) {
+            return;
+        }
+        $currId = 0;
+        $values = [];
+        foreach ($this as $k => $v) {
+            if ('id' == $k) {
+                $currId = (int)$v;
+                continue;
+            }
+            $values[$k . ' := ' . $k] = $v;
+        }
+        $sql = 'UPDATE ' . static::TABLE .
+            ' SET ' .
+            implode(',', array_keys($values)) .
+            ' WHERE id = ' . $currId;
+        $db = Db::instance();
+        $db->execute($sql, $values);
     }
-    $currId = 0;
-    $values = [];
-    foreach ($this as $k => $v) {
-      if ('id' == $k) {
-        $currId = (int)$v;
-        continue;
-      }
-      $values[$k . ' := ' . $k] = $v;
-    }
-    $sql = 'UPDATE ' . static::TABLE .
-      ' SET ' .
-      implode(',', array_keys($values)) .
-      ' WHERE id = ' . $currId;
-    $db = Db::instance();
-    $db->execute($sql, $values);
-  }
 }
 
--- index.php
+++ index.php
@@ -1,10 +1,19 @@
- <?php
+<?php
 
 require __DIR__ . '/autoload.php';
 
 $url = $_SERVER['REQUEST_URI'];
 
-$contoller = new \App\Controlers\News();
+if (isset($_GET['ctrl'])){
+  $ctrl = $_GET['ctrl'] ?: 'News';
+  $ctrl = '\App\Controlers' . '\\' . $ctrl;
+
+}else{
+  $ctrl = '\App\Controlers\News';
+}
+
+$contoller = new $ctrl();
+//$contoller = new \App\Controlers\News();
 
 $action = $_GET['action'] ?: 'Index';
 
